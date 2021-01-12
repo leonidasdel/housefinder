60aaa04535553527f82bc867b4c67695a62aed5a
leonidasdel <55215042+leonidasdel@users.noreply.github.com>
2021-01-12 18:58:45 +0200

    Update SellHouse.component.jsx

--- client/src/components/sellHouse/SellHouse.component.jsx
+++ client/src/components/sellHouse/SellHouse.component.jsx
@@ -1,7 +1,19 @@
-import React,{ useEffect} from 'react';
+import React,{ useEffect,useState } from 'react';
 import "./SellHouse.styles.scss"
 
+const google = window.google = window.google ? window.google : {} 
+var geocoder;
+var options = {
+    componentRestrictions: {country: "gb"}
+   };
+var formData;
 function SellHouse(props){
+
+    useEffect(()=> {
+        let autocomplete = new google.maps.places.Autocomplete(document.getElementById('street_address'), options);
+        geocoder = new google.maps.Geocoder();
+        formData = new FormData();
+    })
 
     const file = document.querySelector('#file');
     let changeFile =  (e) => {
@@ -16,22 +28,58 @@ function SellHouse(props){
         if (extFile=="jpg" || extFile=="jpeg" || extFile=="png"){
             document.getElementById("helper-notify").style.display ="none"
             document.querySelector('.file-name').textContent = fileName;
+            console.log(e.target.files[0])
+            formData = new FormData();
+            formData.append('house',e.target.files[0])
+            console.log(formData)
 
         }else{
             alert("Only jpg/jpeg and png files are allowed!");
-        }   
+        } 
+        
        
 };
 
+    const handleSubmit = (e) => {
+        console.log(e)
+        e.preventDefault();
+       
+        let objData = {
+            "firsName": e.target[0].value
+        }
+        formData.append('objArr', JSON.stringify( objData))
+        for (var key of formData.entries()) {
+            console.log(key[0] + ', ' + key[1]);
+        }
+    }
+
+    const autofillAddress = (e) => {
+        geocoder.geocode({ address: e.target.value }, function(results, status) {
+            let possibleAddresses = results[0].address_components
+            console.log(possibleAddresses)
+            for (let i =0;i<possibleAddresses.length;i++){
+                if(possibleAddresses[i].types[0] === "postal_town"){
+                    document.getElementById("city").value = possibleAddresses[i].long_name
+                }
+                else if(possibleAddresses[i].types[0] === "country"){
+                    document.getElementById("country").value = possibleAddresses[i].long_name
+                }
+                else if(possibleAddresses[i].types[0] === "postal_code"){
+                    document.getElementById("postal_code").value = possibleAddresses[i].long_name
+                }
+            }
+        })
+    }
 
     return (
     <section className="container_page_form">
         <div id="picture"></div>
         <div id="form_sell" className="form_sell">
             <div className="form_sell_container">
+            <form onSubmit={(e) => handleSubmit(e)}>
                 <div className="form_sell_container_row">
                     <div className="form_sell_container_row_item">
-                    <input id="first_name" type="text" className="input-field_text" placeholder="First Name"/>
+                    <input id="first_name" type="text" className="input-field_text" placeholder="First Name"  />
                         <label className="input-field_label" htmlFor="first_name">First Name</label>
                     </div>
                     <div className="form_sell_container_row_item">
@@ -51,7 +99,7 @@ function SellHouse(props){
                 </div>
                 <div className="form_sell_container_row">
                     <div className="form_sell_container_row_item">
-                    <input id="street_address" type="text" className="input-field_text" placeholder="Street Address"/>
+                    <input id="street_address" type="text" className="input-field_text" placeholder="Street Address" onBlur={(e) => autofillAddress(e)}/>
                         <label className="input-field_label" htmlFor="street_address">Street Address</label>
                     </div>
                     <div className="form_sell_container_row_item">
@@ -89,6 +137,7 @@ function SellHouse(props){
                         <label className="input-field_label" htmlFor="price">Price</label>
                     </div>
                 </div>
+                
                 <div class="file-input">
                     <input type="file" id="file" class="file" accept="image/x-png,image/jpg,image/jpeg"  onChange={(e) =>changeFile(e)}/>
                     <label for="file">
@@ -97,8 +146,8 @@ function SellHouse(props){
                     </label>
                      <span id="helper-notify">(Only accepts png,jpeg and jpg)</span>
                 </div>
-
-
+                <button type="submit">Submit</button>
+                </form>
                 
             </div>
         </div>
