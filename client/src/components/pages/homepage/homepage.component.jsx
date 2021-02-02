5eac5f0e7f69c72318046348d95119d921402302
leonidasdel <55215042+leonidasdel@users.noreply.github.com>
2021-02-02 20:03:08 +0200

    remove uneccary imges and clean the code

--- client/src/components/pages/homepage/homepage.component.jsx
+++ client/src/components/pages/homepage/homepage.component.jsx
@@ -3,17 +3,13 @@ import "./homepage.styles.scss"
 import {useSelector,useDispatch} from 'react-redux'
 import * as action from '../../redux/actions';
 
-// initialize google maps
+// initialize google maps and restrict results to cities and uk
 const google = window.google = window.google ? window.google : {} 
 var options = {
     types: ['(cities)'],
     componentRestrictions: {country: "gb"}
    };
 
-
-// redux actions
-// const searchTerm  =  useSelector(state => state.searchReducer)
-// const dispatch = useDispatch()
 
 function HomePage(props)  {
  
@@ -23,17 +19,15 @@ const dispatch = useDispatch()
     
     
     
-      useEffect(()=>{
-        let autocomplete = new google.maps.places.Autocomplete(document.getElementById('address_input'), options)});
-        // autocomplete.addListener("place_changed", handlePlaceSelect)}
-        //     )
-       
-        
-     
-        function handleSubmit(e){
+      useEffect(()=>
+      {
+        let autocomplete = new google.maps.places.Autocomplete(document.getElementById('address_input'), options)
+      });
+
+      const handleSubmit = (e) => {
             e.preventDefault()
-            let text = document.getElementById('address_input').value
-            dispatch(action.changeSearchText(text))
+            let address = document.getElementById('address_input').value
+            dispatch(action.changeSearchText(address))
             props.history.push('/houses');
         }
 
@@ -42,10 +36,10 @@ const dispatch = useDispatch()
             <main className="mainPage">
                 <section className="mainPage_search">
                     <h1 className="mainPage_search_title">
-                        Εύρεση Διαμερισμάτων<span className="mainPage_search_title_span">Με ένα μόνο κλικ!</span>
+                       Search for Apartments<span className="mainPage_search_title_span">With just one click!</span>
                     </h1>
                     <form onSubmit={(e) => handleSubmit(e)} action="" className="mainPage_search_bar">
-                        <input   type="search" id="address_input" placeholder="Manchester, Blackpool, Liverpool..." className="mainPage_search_bar_input" />
+                        <input type="search" id="address_input" placeholder="Manchester, Blackpool, Liverpool..." defaultValue="London" className="mainPage_search_bar_input" />
                         <button type="submit" class="btn btn-success">
                             <i type="submit" className="fas fa-search"></i>
                         </button>
